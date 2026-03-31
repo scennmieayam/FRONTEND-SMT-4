0ae03ec0adebeca3cbe3a1de947498b42b275967
scennmieayam <scendy17@gmail.com>
2026-03-31 09:28:57 +0700

    pertemuan4

---
+++ pertemuan4/ecommerce_dinamis/app/api/products/route.js
@@ -0,0 +1,7 @@
+import { connect } from "@/lib/db"
+
+export async function GET() {
+    const db = await connect()
+    const [rows] = await db.execute("SELECT * FROM products")
+    return Response.json(rows)
+}
---
+++ pertemuan4/ecommerce_dinamis/lib/db.js
@@ -0,0 +1,11 @@
+import mysql from 'mysql2/promise'
+
+export async function connect() {
+    const connection = await mysql.createConnection({
+        host: 'localhost',
+        user:'root',
+        password:'scendy',
+        database: 'ecommerce_db'
+    })
+    return connection
+}
